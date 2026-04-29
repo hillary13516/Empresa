df087c8ca8b29bcdc08abc328e9e5709e0ec625d
hillary13516 <maria.hillary@academico.ifpb.edu.br>
2026-04-29 11:11:37 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.image("teste.png")
-st.write("Site Dinaldo Jorge")
-st.link_button("Acessar", "https://www.dinaldo.com.br")
+st.image("FOTO.EMPRESA.png")
+st.write("NOME MARIA HILLARY")
+st.image("foto.png.jpeg")
+st.write("MARIA HILLARY, 17 ANOS. ESTUDO NO IFPB CAMPUS ITABAIANA, FAÇO O CURSO DE INFORMÁTICA E ESTOU NO 3° ANO.")
+st.image("ZAP.png.")
+st.link_button("Acessar", "https://web.whatsapp.com/")
