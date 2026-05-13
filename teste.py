28b0a40daf569d6607cd0d9f2c7f2dbec4272b6e
hillary13516 <maria.hillary@academico.ifpb.edu.br>
2026-05-13 11:38:34 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,84 @@
 import streamlit as st
+import base64
 
-st.image("FOTO.EMPRESA.webp")
-st.write("NOME MARIA HILLARY")
-st.image("foto.png.jpeg")
-st.write("MARIA HILLARY, 17 ANOS. ESTUDO NO IFPB CAMPUS ITABAIANA, FAÇO O CURSO DE INFORMÁTICA E ESTOU NO 3° ANO.")
-st.image("ZAP.png")
-st.link_button("Acessar", "https://web.whatsapp.com/")
+
+st.set_page_config(page_title="Perfil", layout="wide")
+
+# FUNÇÃO base64
+def get_base64_image(path):
+    with open(path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+img_base64 = get_base64_image("star.png")
+zap_base64 = get_base64_image("zap2026.png")
+
+
+col1, col2, col3 = st.columns([1,2,1])
+
+with col2:
+    st.markdown(f"""
+        <div style="text-align: center; margin-bottom: 50px;">
+            <a href="https://starlink.com/" target="_blank">
+                <img src="data:image/png;base64,{img_base64}" 
+                     width="320" 
+                     style="border-radius:12px;">
+            </a>
+        </div>
+    """, unsafe_allow_html=True)
+
+# LAYOUT PRINCIPAL
+col_left, col_right = st.columns([3,1])
+
+with col_left:
+    st.markdown("""
+    <div style='margin-bottom:30px; font-size:30px;'>
+        <b>Nome Maria Hillary</b>
+    </div>
+    """, unsafe_allow_html=True)
+
+    # subcolunas
+    subcol1, subcol2 = st.columns([1,4])
+
+    # IMAGEM (centralizada verticalmente)
+    with subcol1:
+        st.markdown("""
+        <div style="
+            display: flex;
+            align-items: center;
+            height: 100%;
+        ">
+        """, unsafe_allow_html=True)
+
+        st.image("hillary2026.png", width=800)
+
+        st.markdown("</div>", unsafe_allow_html=True)
+
+    # TEXTO
+    with subcol2:
+        st.markdown("""
+        <div style="
+            text-align: justify;
+            font-size: 20px;
+            line-height: 2.0;
+            width: 100%;
+            max-width: none;
+        ">
+            <b>Sobre Hillary:<br>
+            Maria Hillary, 17 anos, estudante do ensino média no IFPB Camppus Itabaiana.
+        </div>
+        """, unsafe_allow_html=True)
+
+    st.markdown("<div style='margin-top:30px;'>", unsafe_allow_html=True)
+    st.link_button("Acessar", "http://lattes.cnpq.br/4494611683890258")
+    st.markdown("</div>", unsafe_allow_html=True)
+
+with col_right:
+    st.empty()
+
+st.markdown(f"""
+    <div style="text-align: center; margin-top: 10px;">
+        <a href="https://wa.me/982134672" target="_blank">
+            <img src="data:image/png;base64,{zap_base64}" width="100">
+        </a>
+    </div>
+""", unsafe_allow_html=True)
